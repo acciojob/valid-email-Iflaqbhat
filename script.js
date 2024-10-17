26f53ea5295e813719a0324762346f533333403b
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-17 10:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function validEmail(str) {
-  //your JS code here.
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+  if (str.length === 0) {
+    return false;
+  }
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2})?$/;
+
+  return emailRegex.test(str);
 }
 
-// Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
